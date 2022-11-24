260af2a5f7d1107cd251b8bde12b37fdc972357d
HoangTranNe <tranbaohoang70@gmail.com>
2022-11-24 21:34:05 +0700

    save

---
+++ Models/cartController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace do_an_web.Models
+{
+    public class cartController : Controller
+    {
+        // GET: cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public List<needtobuy> makecart()
+        {
+            List<needtobuy> gioHang = Session["GioHang"] as List<needtobuy>;
+            //Nếu giỏ hàng chưa tồn tại thì tạo mới và đưa vào Session
+            if (gioHang == null)
+            {
+                gioHang = new List<needtobuy>();
+                Session["GioHang"] = gioHang;
+            }
+            return gioHang;
+        }
+        public void addtocart(int id_products)
+        {
+            //Lấy giỏ hàng hiện tại
+            List<needtobuy> gioHang = makecart();
+            //Kiểm tra xem có tồn tại mặt hàng trong giỏ hay chưa
+            //Nếu có thì tăng số lượng lên 1, ngược lại thêm vào giỏ
+            needtobuy sanPham = gioHang.FirstOrDefault(s => s.id_product == id_products);
+            if (sanPham == null) //Sản phẩm chưa có trong giỏ
+            {
+                sanPham = new needtobuy(id_products);
+                gioHang.Add(sanPham);
+            }
+            else
+            {
+                sanPham.quantity++; //Sản phẩm đã có trong giỏ thì tăng số lượng lên 1
+            }
+            return RedirectToAction("Details", "products", new { id = id_products });
+        }
+    }
+}
---
+++ Models/needtobuy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace do_an_web.Models
+{
+    public class needtobuy
+    {
+        webClothesEntities buy = new webClothesEntities();
+        public int id_product { get; set; }
+        public string name { get; set; }
+        public string images { get; set; }
+        public double price { get; set; }
+        public double quantity { get; set; }
+        public double total ()
+        { 
+            return price * quantity;
+        }
+        public int price_total { get; set; }
+        public int quantity_total { get; set;}
+
+        public needtobuy(int products)
+        {
+            this.id_product= products;
+            var product = buy.products.Single(s => s.id_products == this.id_product);
+            this.name = product.name;
+            this.images = product.images;
+            this.price = double.Parse(product.price.ToString());
+            this.quantity = 1;
+        }
+    }
+}
