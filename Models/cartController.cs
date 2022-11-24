413445ce0d7ac8386f691c5b74314f9136adec12
HoangTranNe <tranbaohoang70@gmail.com>
2022-11-25 01:02:05 +0700

    thêm Microsoft.AspNetCore.Session và Microsoft.Extensions.Caching.Memory

--- Models/cartController.cs
+++ Models/cartController.cs
@@ -15,26 +15,26 @@ namespace do_an_web.Models
         }
         public List<needtobuy> makecart()
         {
-            List<needtobuy> cart = Session["cart"] as List<needtobuy>;
+            List<needtobuy> carts = Session["cart"] as List<needtobuy>;
             //Nếu giỏ hàng chưa tồn tại thì tạo mới và đưa vào Session
-            if (cart == null)
+            if (carts == null)
             {
-                cart = new List<needtobuy>();
-                Session["cart"] = cart;
+                carts = new List<needtobuy>();
+                Session["cart"] = carts;
             }
-            return cart;
+            return carts;
         }
         public RedirectToRouteResult addtocart(int id_products)
         {
             //Lấy giỏ hàng hiện tại
-            List<needtobuy> cart = makecart();
+            List<needtobuy> carts = makecart();
             //Kiểm tra xem có tồn tại mặt hàng trong giỏ hay chưa
             //Nếu có thì tăng số lượng lên 1, ngược lại thêm vào giỏ
-            needtobuy product = cart.FirstOrDefault(s => s.id_product == id_products);
+            needtobuy product = carts.FirstOrDefault(s => s.id_product == id_products);
             if (product == null) //Sản phẩm chưa có trong giỏ
             {
                 product = new needtobuy(id_products);
-                cart.Add(product);
+                carts.Add(product);
             }
             else
             {
@@ -45,32 +45,38 @@ namespace do_an_web.Models
         private int caculate_total_quantity()
         {
             int quantity_total = 0;
-            List<needtobuy> cart= makecart();
-            if (cart != null)
+            List<needtobuy> carts= makecart();
+            if (carts != null)
             {
-                quantity_total=cart.Sum(s => s.quantity);
+                quantity_total=carts.Sum(s => s.quantity);
             }
             return quantity_total;
         }
         private double caculate_total_price()
         {
             double total_price = 0;
-            List<needtobuy> cart = makecart();
-            if (cart != null)
-                total_price = cart.Sum(s => s.price);
+            List<needtobuy> carts = makecart();
+            if (carts != null)
+                total_price = carts.Sum(s => s.price);
             return total_price;
         }
         public ActionResult showcart()
         {
-            List<needtobuy> cart = makecart();
+            List<needtobuy> carts = makecart();
             //Nếu giỏ hàng trống thì trả về trang ban đầu
-            if (cart == null || cart.Count == 0)
+            if (carts == null || carts.Count == 0)
             {
                 return RedirectToAction("Index", "products");
             }
             ViewBag.TongSL = caculate_total_quantity();
             ViewBag.TongTien = caculate_total_price();
-            return View(cart); //Trả về View hiển thị thông tin giỏ hàng
+            return View(carts); //Trả về View hiển thị thông tin giỏ hàng
+        }
+        public ActionResult cartpartial()
+        {
+            ViewBag.TongSL = caculate_total_quantity();
+            ViewBag.TongTien = caculate_total_price();
+            return PartialView();
         }
     }
 }
